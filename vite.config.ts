15b1f0956ea9eba4aba483e31a9d66f2983ce485
lwl <1181102955@qq.com>
2021-07-05 23:18:13 +0800

    build: 新增vite 的 vue-ts 构建模板

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 配置tsx
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: "import { h } from 'vue'"
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@comps': resolve(__dirname, 'src/components'),
+    }
+  }
+})
