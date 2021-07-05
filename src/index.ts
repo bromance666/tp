b421cfb2afd3c34169d4f10a8fb1a878de9d8b88
lwl <1181102955@qq.com>
2021-07-05 23:24:36 +0800

    build: 新增vue所有组件插件导出入口

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import * as components from "./components/index"
+console.log(components)
+export default {
+  install: (instance: any) => {
+    for (let componentKey in components) {
+      // @ts-ignore
+      instance.use((components)[componentKey])
+    }
+  }
+}
