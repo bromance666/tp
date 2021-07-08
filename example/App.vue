ecf9617dbd7bee74a4850c98c58ae4047982d01b
xiangyu <luoxy@luoxydeMacBook-Pro.local>
2021-07-08 12:04:00 +0800

    fix(path): resolve dev path bug

--- example/App.vue
+++ example/App.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import TpButton from '@comps/button/index';
+  import TpButton from '@comps/button';
 
   import style from 'index.less';
 
