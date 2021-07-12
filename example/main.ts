54150993cf55d6f93a0bcd7ebbe69e10102a90a4
lwl <1181102955@qq.com>
2021-07-12 23:17:54 +0800

    feat: 修改全局组件注入

--- example/main.ts
+++ example/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { TpButtonTest, TpButton } from '../lib';
+// import { TpButtonTest, TpButton } from '../lib';
+import components from './index';
 import '../lib/style/button.css';
 import router from './router/index';
 const app = createApp(App);
-
 app.use(router);
-app.use(TpButtonTest);
-app.use(TpButton);
+app.use(components);
+// app.use(TpButtonTest);
+// app.use(TpButton);
 app.mount('#app');
