2e1256f2a253fd58cb5eef542b2c0575cd2a4d32
lwl <1181102955@qq.com>
2021-07-12 23:13:48 +0800

    feat: 新增vue-jsx配置使vite支持jsx v-model等语法

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx  from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 import devConfig from './env/vite.dev'
 import proConfig from './env/vite.prod'
-
 const envConfig = process.env.NODE_ENV === 'dev' ? devConfig : proConfig
 
 // console.log(envConfig)
 
 const finnalConfig: Partial<UserConfigExport> = {
-  plugins: [vue({
-  })],
+  plugins: [vueJsx ({
+    transformOn: true
+  }), vue({})],
   // 配置tsx
   esbuild: {
     jsxFactory: 'h',
