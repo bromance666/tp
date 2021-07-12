d0be37036dab7e05a226de062360bdf607f0169d
lwl <1181102955@qq.com>
2021-07-12 23:21:12 +0800

    feat: 增加example button组件引用tsx组件并使用vmodel、插槽功能

--- example/views/button/index.tsx
+++ example/views/button/index.tsx
@@ -1,10 +1,26 @@
-import { defineComponent } from 'vue';
+import { defineComponent, getCurrentInstance, ref, ComponentInternalInstance } from 'vue';
+// import { TpButton } from '../../../lib'
 
 const ButtonView = defineComponent({
   name: 'ButtonView',
   props: {},
-  setup() {
-    return () => <button>按钮</button>;
+  setup(props) {
+    const instance = getCurrentInstance();
+    const TpButton: any = instance.appContext.components.TpButton;
+    const ButtonX = TpButton as any;
+    const val = ref('12312321');
+    const scopedSlots = {};
+
+    return () => (
+      <div>
+        {val.value}
+        <input type="text" v-model={val.value} placeholder={val.value} />
+        {/* <TpButton v-slots={{
+            default: (n: string) => <span>这是按钮</span>,
+          }}></TpButton> */}
+        <TpButton>这是按钮</TpButton>
+      </div>
+    );
   },
 });
 
