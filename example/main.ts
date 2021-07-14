afabdf7b7b2f07b54a1932d148f724e5494b2af1
lwl <1181102955@qq.com>
2021-07-14 23:32:05 +0800

    feat: 可以根据打包方式选择启动vue项目

--- example/main.ts
+++ example/main.ts
@@ -1,12 +1,30 @@
+/**
+ * vite打包
+ *
+ **/
 import { createApp } from 'vue';
 import App from './App.vue';
-// import { TpButtonTest, TpButton } from '../lib';
 import components from './index';
-import '../lib/style/index.css';
+import '../dist/styles/index.less';
 import router from './router/index';
 const app = createApp(App);
 app.use(router);
 app.use(components);
-// app.use(TpButtonTest);
-// app.use(TpButton);
 app.mount('#app');
+
+/**
+ * esbuild打包
+ */
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// // import { TpButtonTest, TpButton } from '../lib';
+// import components from './index';
+// import router from './router/index';
+
+// import '../dist/styles/index.less';
+// import { createTp } from '../lib/'
+// const app = createApp(App);
+// app.use(router);
+// app.use(components);
+// app.use(createTp);
+// app.mount('#app');
